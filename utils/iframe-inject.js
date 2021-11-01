805919b2cc29dffe3be9fe22a241c66b71b00830
wangzhipeng <wangzhipeng@wxchina.com>
2021-11-01 17:36:19 +0800

    注入手机端模拟js

--- utils/iframe-inject.js
+++ utils/iframe-inject.js
@@ -3,13 +3,14 @@ function injectIframe (content) {
   css.type = 'text/css';
   css.innerHTML = 'body::-webkit-scrollbar { width: 0 }';
   content.document.getElementsByTagName('head')[0].appendChild(css);
+  var script = content.document.createElement('script')
+  script.src = '../../../../lib/vant-touch-emulator.js'
+  content.document.getElementsByTagName('head')[0].appendChild(script)
   content.xpe_callEvent = function (name, params) {
     content.parent.postMessage({ key: name, data: params }, location.origin)
   }
   content.addEventListener('message', (e) => {
     if (!e.data.key) return
-    console.log(content)
     content.xpe_reciveData(e.data, e.data.key)
   }, false)
 }
-console.log('inject js')
